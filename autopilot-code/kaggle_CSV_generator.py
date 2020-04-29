ac251e333ff910583bd655dc915e4844864a89ec
tham yik foong <tham_yikfoong@outlook.com>
2020-04-29 22:25:26 +0100

    update CSV generator

--- autopilot-code/kaggle_CSV_generator.py
+++ autopilot-code/kaggle_CSV_generator.py
@@ -6,10 +6,13 @@ import numpy as np
 import pandas as pd
 import re
 
+def round_to(x, base=5):
+    return base * round(x/base)
+
 def main():
     data_dir = 'test_data'
     file_list = os.listdir(data_dir)
-    model = load_model('lane_navigation_check_LD_Objects.h5')
+    model = load_model('lane_navigation_final_LD_Objects.h5')
     predictions = []
     image_id = []
 
@@ -26,8 +29,8 @@ def main():
     predictions = pd.DataFrame(np.concatenate(predictions), columns=['angle', 'speed'])
     output = pd.concat([image_id, predictions], axis=1, sort=False)
 
-    output.angle = (output.angle - 50)/80
-    output.speed = (output.speed - 0)/35
+    output.angle = (round_to(output.angle) - 50)/80
+    output.speed = round((output.speed - 0)/35)
     output.sort_values(by=['image_id'], inplace=True)
     output.to_csv('LD-Objects-Submission.csv', index=False)
 
